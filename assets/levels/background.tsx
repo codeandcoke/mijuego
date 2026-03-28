edf02149b33034de883bfef9982c014ad9363fb1
Santiago Faci <santi@codeandcoke.com>
2026-03-28 20:59:35 +0100

    Tiled Map y Cámara

--- assets/levels/background.tsx
+++ assets/levels/background.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="background" tilewidth="32" tileheight="32" tilecount="102" columns="6">
  <image source="tileset-platformer.png" width="192" height="544"/>
+ <tile id="58">
+  <properties>
+   <property name="type" value="spike"/>
+  </properties>
+ </tile>
 </tileset>
--- core/src/main/java/com/svalero/mijuego/manager/LogicManager.java
+++ core/src/main/java/com/svalero/mijuego/manager/LogicManager.java
@@ -5,9 +5,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Disposable;
-import com.svalero.mijuego.domain.Character;
 import com.svalero.mijuego.domain.Enemy;
+import com.svalero.mijuego.domain.Item;
 import com.svalero.mijuego.domain.Player;
 import com.svalero.mijuego.screen.MainMenuScreen;
 
@@ -16,18 +17,18 @@ import static com.svalero.mijuego.domain.Character.State.RUNNING_LEFT;
 public class LogicManager implements Disposable {
 
     protected Player player;
-    // TODO Gestionar todos los enemigos como un array
-    protected Enemy enemy;
+    protected Array<Enemy> enemies;
+    protected Array<Item> items;
 
     private Music gameMusic;
 
     public LogicManager() {
+        this.enemies = new Array<>();
+        this.items = new Array<>();
     }
 
     public void load() {
         player = new Player(ResourceManager.getRegion("player_idle_right"), "player_run_left", "player_run_right");
-        enemy = new Enemy(ResourceManager.getRegion("green_bubble_left"), new Vector2(1000, 100),
-            RUNNING_LEFT, "green_bubble_left", "green_bubble_right");
 
         // FIXME
 //        if (ConfigurationManager.isMusicEnabled())
@@ -40,8 +41,9 @@ public class LogicManager implements Disposable {
         player.handleInput(dt);
         player.update(dt);
 
-        // TODO Gestionar todos los enemigos como un array
-        enemy.update(dt);
+        for (Enemy enemy : enemies) {
+            enemy.update(dt);
+        }
 
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
             ((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenuScreen());
