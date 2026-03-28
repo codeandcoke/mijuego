c148b62bc98947f10c39ea5669e46266e210f228
Santiago Faci <santi@codeandcoke.com>
2026-03-28 18:30:55 +0100

    Nuevos assets para el juego. ResourceManager terminado. Animaciones funcionando

--- core/src/main/java/com/svalero/mijuego/domain/Character.java
+++ core/src/main/java/com/svalero/mijuego/domain/Character.java
@@ -1,26 +1,65 @@
 package com.svalero.mijuego.domain;
 
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.svalero.mijuego.manager.ResourceManager;
 import lombok.Data;
 
 @Data
 public abstract class Character {
 
-    protected Texture texture;
+    protected TextureRegion currentFrame;
     protected Vector2 position;
     protected Rectangle rectangle;
+    protected float stateTime;
 
-    public Character(Texture texture, Vector2 position) {
-        this.texture = texture;
+    public enum State {
+        IDDLE_LEFT, IDDLE_RIGHT, RUNNING_RIGHT, RUNNING_LEFT, JUMPING
+    }
+    public State state;
+    protected Animation<TextureRegion> runningLeftAnimation;
+    protected Animation<TextureRegion> runningRightAnimation;
+
+    private String rightAnimationName;
+    private String leftAnimationName;
+
+    public Character(TextureRegion currentFrame, Vector2 position, State initialState, String leftAnimationName, String rightAnimationName) {
+        this.currentFrame = currentFrame;
         this.position = position;
-        rectangle = new Rectangle(position.x, position.y, texture.getWidth(), texture.getHeight());
+        rectangle = new Rectangle(position.x, position.y, currentFrame.getRegionWidth(), currentFrame.getRegionHeight());
+
+        state = initialState;
+        this.rightAnimationName = rightAnimationName;
+        this.leftAnimationName = leftAnimationName;
+        runningLeftAnimation = new Animation<>(0.15f, ResourceManager.getRegions(leftAnimationName));
+        runningRightAnimation = new Animation<>(0.15f, ResourceManager.getRegions(rightAnimationName));
     }
 
     public void draw(Batch batch) {
-        batch.draw(texture, position.x, position.y);
+        batch.draw(currentFrame, position.x, position.y);
+    }
+
+    public void update(float dt) {
+        stateTime += dt;
+
+        // Actualizar qué textura toca pintar
+        switch (state) {
+            case IDDLE_LEFT:
+                currentFrame = ResourceManager.getRegion(leftAnimationName);
+                break;
+            case IDDLE_RIGHT:
+                currentFrame = ResourceManager.getRegion(rightAnimationName);
+                break;
+            case RUNNING_LEFT:
+                currentFrame = runningLeftAnimation.getKeyFrame(stateTime, true);
+                break;
+            case RUNNING_RIGHT:
+                currentFrame = runningRightAnimation.getKeyFrame(stateTime, true);
+                break;
+        }
     }
 
     public float getTopPosition() {
--- core/src/main/java/com/svalero/mijuego/domain/Player.java
+++ core/src/main/java/com/svalero/mijuego/domain/Player.java
@@ -2,38 +2,40 @@ package com.svalero.mijuego.domain;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Disposable;
 import lombok.Data;
 
+import static com.svalero.mijuego.domain.Character.State.*;
 import static com.svalero.mijuego.util.Constants.PLAYER_SPEED;
 
 @Data
 public class Player extends Character implements Disposable {
 
-    public Player(Texture texture) {
-        super(texture, new Vector2(100, 100));
+    public Player(TextureRegion texture, String leftAnimationName, String rightAnimationName) {
+        super(texture, new Vector2(100, 100), IDDLE_RIGHT, leftAnimationName, rightAnimationName);
     }
 
     public void handleInput(float delta) {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            state = RUNNING_LEFT;
             position.x -= PLAYER_SPEED * delta;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            state = RUNNING_RIGHT;
             position.x += PLAYER_SPEED * delta;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            state = JUMPING;
+        } else {
+            if (state != IDDLE_LEFT && state != IDDLE_RIGHT)
+                state = state == RUNNING_LEFT ? IDDLE_LEFT : IDDLE_RIGHT;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            position.y += PLAYER_SPEED * delta;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            position.y -= PLAYER_SPEED * delta;
-        }
+
         rectangle.setPosition(position.x, position.y);
     }
 
     @Override
     public void dispose() {
-        texture.dispose();
+
     }
 }
